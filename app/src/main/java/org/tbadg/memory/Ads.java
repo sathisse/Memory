2d30b86325dfb25e9655ade9245ab750f5c7fb73
Scott A Thisse <scottt@centripetalfusion.com>
2015-11-07 09:02:05 -0500

    Add Ads class

---
+++ app/src/main/java/org/tbadg/memory/Ads.java
@@ -0,0 +1,74 @@
+package org.tbadg.memory;
+
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+class Ads {
+
+    private static final String TAG = "Ads";
+
+    private AdView mAdView = null;
+
+
+    //
+    // Constructor:
+    //
+
+    Ads(View adView) {
+        mAdView = (AdView) adView;
+    }
+
+    //
+    // Public methods:
+    //
+
+    public void showAd() {
+        Log.v(TAG, "in showAd()");
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Log.v(TAG, "in onAdLoaded()");
+            }
+
+            public void onAdFailedToLoad(int errorCode) {
+                Log.v(TAG, "in onAdFailedToLoad()");
+            }
+
+            public void onAdOpened() {
+                Log.v(TAG, "in onAdOpened()");
+            }
+
+            public void onAdClosed() {
+                Log.v(TAG, "in onAdClosed()");
+            }
+
+            public void onAdLeftApplication() {
+                Log.v(TAG, "in onAdLeftApplication()");
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("my_id_num").build();
+        mAdView.loadAd(adRequest);
+    }
+
+    public void pause() {
+        Log.v(TAG, "in pause()");
+        mAdView.pause();
+    }
+
+    public void resume() {
+        Log.v(TAG, "in resume()");
+        mAdView.resume();
+    }
+
+    public void destroy() {
+        Log.v(TAG, "in destroy()");
+        mAdView.pause();
+        mAdView.destroy();
+    }
+}
